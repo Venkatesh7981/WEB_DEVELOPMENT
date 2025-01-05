0c1ecc5917cf51fe41fa81520becc80e47d426ac
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2025-01-05 18:33:04 +0530

    Add files via upload
    currency

---
+++ CURRENCY_MINI_PROJECT/app.js
@@ -0,0 +1,65 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const from = fromCurr.value.toLowerCase();
+  const to = toCurr.value.toLowerCase();
+  const URL = `${BASE_URL}/${from}/${to}.json`;
+
+  const response = await fetch(URL);
+  const data = await response.json();
+  const rate = data[to];
+
+  if (!rate) {
+    msg.innerText = "Exchange rate not available.";
+    return;
+  }
+
+  const finalAmount = (amtVal * rate).toFixed(2);
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  const currCode = element.value;
+  const countryCode = countryList[currCode];
+  const newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  const img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
